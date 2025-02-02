d518d591305acb4fb5de58d95fc5c2bd886ff8ed
khashayar nourian <knourian.dev@gmail.com>
2025-02-02 12:15:42 +0330

    updated versioning of build

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,32 @@
 const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
+const { DefinePlugin } = require('webpack');
+const fs = require('fs');
+
+class UpdateManifestVersionPlugin {
+    apply(compiler) {
+        compiler.hooks.afterEmit.tap('UpdateManifestVersionPlugin', (compilation) => {
+            const manifestPath = path.resolve(__dirname, 'dist', 'manifest.json');
+
+            // Check if the copied manifest.json exists in the dist folder
+            if (fs.existsSync(manifestPath)) {
+                const manifest = JSON.parse(fs.readFileSync(manifestPath, 'utf-8'));
+
+                const timestamp = Date.now(); // e.g., 1612325400000
+                const newVersion = `${manifest.version}.${Math.floor(timestamp / 1000) % 65536}`;
+                manifest.version = newVersion;
+
+                // Write the updated manifest.json back to the dist folder
+                fs.writeFileSync(manifestPath, JSON.stringify(manifest, null, 2));
+                console.log(`Manifest version updated in dist to: ${newVersion}`);
+            }
+        });
+    }
+}
+
 
 module.exports = {
+    devtool: 'source-map',
     entry: {
         background: './background.ts',
         content: './content.ts',
@@ -29,9 +54,11 @@ module.exports = {
                 { from: './README.md', to: 'README.md' },
                 { from: './manifest.json', to: 'manifest.json' },
                 { from: './popup/popup.html', to: 'popup/popup.html' },
+                { from: './popup/popup.css', to: 'popup/popup.css' },
                 { from: './icons/', to: 'icons' }
             ],
         }),
+        new UpdateManifestVersionPlugin(),
     ],
     mode: 'development',
 };
